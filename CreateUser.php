522a853a1dd36c6eaf359050325dd1eebb7c773e
Anjali Pare <anjali.pare@gmail.com>
2019-11-05 19:51:52 -0600

    Got the PHP file to work

--- CreateUser.php
+++ CreateUser.php
@@ -1,4 +1,29 @@
 <?php
-echo "<link rel='stylesheet' type='text/css' href='style.css' />";
+
+
+ $username = $_POST["username"];
+ if(empty($username))
+ {
+   echo "You cannot leave the text field blank";
+ }
+ else
+ {
+   $mysqli = new mysqli("mysql.eecs.ku.edu", "anjalipare", "ohVeequ3", "anjalipare");
+   if ($mysqli->connect_errno) {
+       printf("Connect failed: %s\n", $mysqli->connect_error);
+       exit();
+   }
+
+     $query = "INSERT INTO Users(user_id) VALUES ('$username')";
+     if ($mysqli->query($query))
+     {
+       echo "New User Created successfully";
+     }
+     else {
+       echo "This user already exists";
+     }
+     $mysqli->close();
+ }
+ /* check connection */
 
 ?>
