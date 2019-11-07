00c98f40419ed3a3d002d5c5b032d66e6180c81b
AnjaliPare <anjali.pare@gmail.com>
2019-11-07 02:20:41 -0600

    Got the posts written by different users to display

--- ViewUserPosts.php
+++ ViewUserPosts.php
@@ -1,22 +1,26 @@
 <?php
-   $flag = false;
    $mysqli = new mysqli("mysql.eecs.ku.edu", "anjalipare", "ohVeequ3", "anjalipare");
    if ($mysqli->connect_errno) {
        printf("Connect failed: %s\n", $mysqli->connect_error);
        exit();
    }
-   echo "<h1>Here are the users:</h1>";
-    $query = "SELECT * FROM Users";
+   $user = $_POST["selectedUser"];
+   echo "Displaying posts from user " . $user . "<br><br>";
+   echo "<table>";
+    $query = "SELECT content FROM Posts WHERE author_id='$user'";
     if ($result = $mysqli->query($query))
     {
+
+      echo "<tr><th>Posts written by user:" . $user . "</th></tr>";
       while ($row = $result->fetch_assoc())
       {
-        echo ($row["user_id"]);
-        echo "<br>";
+        echo "<tr><td>" . $row["content"] . "</td></tr>";
       }
-    }
 
+    }
+ echo "</table>";
  /* check connection */
     $mysqli->close();
+    echo "<br><br><br>";
     echo "<br><a href='AdminHome.html'>Admin Home </a>";
 ?>
